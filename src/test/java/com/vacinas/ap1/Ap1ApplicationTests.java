2eb7ff62b5555d2ecfef104269272966d5bc4de2
Ademario <ademarioteles@hotmail.com>
2023-10-07 14:13:37 -0300

    Subindo projeto inicial api1

---
+++ src/test/java/com/vacinas/ap1/Ap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacinas.ap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
